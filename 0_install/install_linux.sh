1536ac03289403a1aa9805e9b03976f4456e6a0d
Iu Ayala <iuayalaportella@gmail.com>
2021-12-23 18:49:59 +0000

    Add automated installation

--- 0_install/install_linux.sh
+++ 0_install/install_linux.sh
@@ -3,5 +3,24 @@
 echo "This installation file should work for:"
 echo "  - Debian based distros (Ubuntu, Pop_OS!, ...)"
 echo "  - Arch-based distros (Arch, Manjaro, ...)"
-echo "  - Fedora/Centos"
+echo "  - Fedora"
 
+# dnf needs to be first because Fedora also has yum
+if [[ ! -z $(which dnf) ]]; then
+    PKM=dnf
+elif [[ ! -z $(which yum) ]]; then
+    PKM=yum
+elif [[ ! -z $(which apt-get) ]]; then
+    PKM=apt-get
+elif [[ ! -z $(which pamac) ]]; then
+    PKM=pamac
+fi
+
+echo "Using package manager: $PKM"
+
+# Install ansible
+echo "sudo $PKM install ansible"
+sudo $PKM install -y ansible
+
+# Run ansible playbook
+ansible-playbook general.yml --ask-become-pass
